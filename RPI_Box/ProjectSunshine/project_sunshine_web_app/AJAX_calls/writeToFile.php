7b6e75dfe5ff4ed965656048e633da33505d8574
ACN\zanoni <zanoni@email.com>
2017-03-15 17:49:37 -0600

    Create directory with proper file premissions.

--- RPI_Box/ProjectSunshine/project_sunshine_web_app/AJAX_calls/writeToFile.php
+++ RPI_Box/ProjectSunshine/project_sunshine_web_app/AJAX_calls/writeToFile.php
@@ -40,6 +40,18 @@
 	fwrite($myfile, $fileString);
 	fclose($myfile);
 
+	$dir = 'COMMANDS';
+
+ 	// create new directory with 744 permissions if it does not exist yet
+ 	// owner will be the user/group the PHP script is run under
+ 	if ( !file_exists($dir) ) {
+    	$oldmask = umask(0);  // helpful when used in linux server  
+     	mkdir ($dir, 0744);
+ 	}
+
+ 	file_put_contents ($dir.'/test.txt', 'Hello File');
+	
+
 	echo json_encode('Delay: ' . $finalTime." File found?? ".$fileFoundBoolean. "Written to file: " . $fileName);
 	
 
