7f7e1c1f3056ac972c5ec8136b7b49d0e9e9833b
Brian O'Dell <brianodellk@gmail.com>
2017-04-10 22:30:02 -0600

    Refactored uninstall, verbose shows dialogs, added restore from backup

--- install.sh
+++ install.sh
@@ -37,6 +37,7 @@ UNINSTALL=0
 FILESONLY=0
 NOBACKUP=0
 HELP=0
+RESTORE=0
 #parse through passed arguments and set values accordingly
 while [[ $# -gt 0 ]]
 do
@@ -58,6 +59,9 @@ case $key in
     -h|--help)
     HELP=1
     ;;
+    -r|--restore)
+    RESTORE=1
+    ;;
     *)
     HELP=1        # unknown option
     ;;
@@ -78,9 +82,12 @@ function showhelp() {
     echo "                 this should be used every time the program"
     echo "                 is run except for the first time"
     echo
+    echo "-r, --restore    restores configs from backup"
+    echo
     echo "-h, --help       to see this message"
     echo " If -u and -f are used together, then the background programs will"
     echo " not be uninstalled, only the files will be reverted"
+    echo " backups located at /opt/sunshine/backups"
     quit
 }
 
@@ -88,11 +95,8 @@ function quit() {
     exit
 }
 
-#function that will undo all changes made by the script
-function undoInstall() {
-    #future implementation will allow user to decide if they only
-    #want to change files, or remove installed packages also
-
+#restoreFiles will restore the configurations made at the initial setup
+function restoreFiles() {
     cp /opt/sunshine/backups/dhcpd.conf.backup /etc/dhcp/dhcpd.conf
     cp /opt/sunshine/backups/isc-dhcp-server.backup /etc/default/isc-dhcp-server
     cp /opt/sunshine/backups/interfaces.backup /etc/network/interfaces
@@ -100,10 +104,66 @@ function undoInstall() {
     cp /opt/sunshine/backups/hostapd.backup /etc/default/hostapd
     cp /opt/sunshine/backups/hostapd.initd.backup /etc/init.d/hostapd
     cp /opt/sunshine/backups/sysctl.conf.backup /etc/sysctl.conf
+    quit
+}
+#function that will undo all changes made by the script
+function undoInstall() {
+
+    if [ $VERBOSE = "1" ]; then
+        echo "Resetting interfaces"
+    fi
+    cp /opt/sunshine/backups/dhcpd.conf.backup /etc/dhcp/dhcpd.conf
+
+    if [ $VERBOSE = "1" ]; then
+        echo "Resetting /etc/dhcp/dhcpd.conf"
+    fi
+    sed -i '/#option domain-name "example.org";/c\option domain-name "example.org";' /etc/dhcp/dhcpd.conf
+    sed -i '/#option domain-name-servers ns1.example.org, ns2.example.org;/c\option domain-name-servers ns1.example.org, ns2.example.org;' /etc/dhcp/dhcpd.conf
+    sed -i '/authoritative;/c\#authoritative;' /etc/dhcp/dhcpd.conf
+    sed -i '/#DHCP ADD/,/#DHCP ADD/d' /etc/dhcp/dhcpd.conf
+
+    if [ $VERBOSE = "1" ]; then
+        echo "Resetting /etc/default/isc-dhcp-server"
+    fi
+    sed -i 'DHCPD_CONF=/etc/dhcp/dhcpd.conf/c\#DHCPD_CONF=/etc/dhcp/dhcpd.conf' /etc/default/isc-dhcp-server
+    sed -i '/INTERFACES="wlan0"/c\INTERFACES=""' /etc/default/isc-dhcp-server
+    service isc-dhcp-server stop
+
+    if [ $VERBOSE = "1" ]; then
+        echo "Resetting /etc/hostapd/hostapd.conf"
+    fi
+    sed -i '/#HOSTAPD ADD/,/#HOSTAPD ADD/d' /etc/hostapd/hostapd.conf
+
+    if [ $VERBOSE = "1" ]; then
+        echo "Resetting /etc/default/hostapd"
+    fi
+    sed -i '/DAEMON_CONF="/etc/hostapd/hostapd.conf"/c\#DAEMON_CONF=""' /etc/default/hostapd
+
+    if [ $VERBOSE = "1" ]; then
+        echo "Resetting /etc/init.d/hostapd"
+    fi
+    sed -i '/DAEMON_CONF=/etc/hostapd/hostapd.conf/c\DAEMON_CONF=' /etc/init.d/hostapd
+
+    if [ $VERBOSE = "1" ]; then
+        echo "Resetting /etc/sysctl.conf"
+    fi
+    sed -i '/net.ipv4.ip_forward=1/c\#net.ipv4.ip_forward=1' /etc/sysctl.conf
+
+    if [ $VERBOSE = "1" ]; then
+        echo "Resetting daemons and settings"
+    fi
+    ifup wlan0
+    ifconfig wlan0 DHCP
+    service hostapd stop
+    service isc-dhcp-server stop
+    update-rc.d hostapd disable
+    update-rc.d isc-dhcp-server disable
 
     cd /var/www/html
     rm -r *
 
+#if set, then only the files will be changed the underlying programs
+#wont be uninstalled
 if [ $FILESONLY = "0" ]; then
     apt-get remove iptables-persistent
     apt-get remove hostapd isc-dhcp-server
@@ -113,13 +173,35 @@ fi
 }
 
 function copyfiles() {
-    echo "copying files to apache directory"
+    #checks current values before rewriting files
+    if [ $VERBOSE = "1" ]; then
+        echo "copying files to apache directory"
+    fi
+    if grep -Fxq "backupdone=1" /var/www/html/INSTALL/config.sun
+    then
+        backupdone=1
+    fi
+    if grep -Fxq "installed=1" /var/www/html/INSTALL/config.sun
+    then
+        installed=1
+    fi
     rsync -r $fileloc/ /var/www/html
     cd /var/www/html
     rm -r .git
     rm .gitignore
-    cd "RPI_Box"/ProjectSunshine
+    cd RPI
     g++ cam.cpp -std=c++11 -o cam
+    g++ blackboard.cpp -std=c++11 -o blackboard
+    cd ..
+
+    #rewrites values based on previous actions
+    if [ $backupdone = "1" ]; then
+        sed -i '/backupdone=0/c\backupdone=1' /INSTALL/config.sun
+    fi
+    if [ $installed = "1" ]; then
+        sed -i '/installed=0/c\installed=1' /INSTALL/config.sun
+    fi
+
     return
 }
 
@@ -130,14 +212,18 @@ if [ $HELP = "1" ]; then
     showhelp
 fi
 
+if [ $RESTORE = "1" ];then
+    restoreFiles
+fi
+
 #Ensure backup files are not overwritten
-if grep -Fxq "backupdone=1" /var/www/html/Install/config.sun
+if grep -Fxq "backupdone=1" /var/www/html/INSTALL/config.sun
 then
     NOBACKUP=1
 fi
 
 #do not do all reinstall acitons if it already completed successfully
-if grep -Fxq "installed=1" /var/www/html/Install/config.sun
+if grep -Fxq "installed=1" /var/www/html/INSTALL/config.sun
 then
     FILESONLY=1
 fi
@@ -157,7 +243,9 @@ else
     #we will copy the files we modify to a directory that is far out of the
     #way. that way we can revert changes later. update first to ensure
     #we have the headers for the new packages
-    echo "installing dependencies"
+    if [ $VERBOSE = "1" ]; then
+        echo "installing dependencies"
+    fi
     cd /
     apt-get update
     apt-get install iptables-persistent
@@ -167,7 +255,10 @@ else
     copyfiles
 #Backs up the files we are using if it wasnt otherwise specified
     if [ $NOBACKUP = "0" ]; then
-        echo "backing up files"
+        if [ $VERBOSE = "1" ]; then
+            echo "backing up files"
+            echo "located at /opt/sunshine/backups"
+        fi
         mkdir /opt/sunshine
         mkdir /opt/sunshine/backups
         cp /etc/dhcp/dhcpd.conf /opt/sunshine/backups/dhcpd.conf.backup
@@ -177,43 +268,70 @@ else
         cp /etc/default/hostapd /opt/sunshine/backups/hostapd.backup
         cp /etc/init.d/hostapd /opt/sunshine/backups/hostapd.initd.backup
         cp /etc/sysctl.conf /opt/sunshine/backups/sysctl.conf.backup
-        sed -i '/backupdone=0/c\backupdone=1' /var/www/html/Install/config.sun
+        sed -i '/backupdone=0/c\backupdone=1' /var/www/html/INSTALL/config.sun
     fi
 
 #Editing the interfaces file
-    echo "Editing interfaces file"
-    echo "#INTERFACE ADD">>/etc/network/interfaces
-    sed -e '/#INTERFACE ADD/{r /var/www/html/Install/interfaceadd.sun' -e 'd}' -i.bak /etc/network/interfaces
+    if [ $VERBOSE = "1" ]; then
+        echo "Editing interfaces file"
+    fi
+    if grep -Fxq "#INTERFACE ADD" /etc/network/interfaces
+    then
+        echo "done"
+    else
+        echo "#INTERFACE ADD">>/etc/network/interfaces
+        sed -e '/#INTERFACE ADD/{r /var/www/html/INSTALL/interfaceadd.sun' -e 'd}' -i.bak /etc/network/interfaces
+    fi
 
 #Editing of /etc/dhcp/dhcpd.conf file
-    echo "Editing /etc/dhcp/dhcpd.conf"
-    sed -i '/option domain-name "example.org";/c\#option domain-name "example.org";' /etc/dhcp/dhcpd.conf
-    sed -i '/option domain-name-servers ns1.example.org, ns2.example.org;/c\#option domain-name-servers ns1.example.org, ns2.example.org;' /etc/dhcp/dhcpd.conf
-    sed -i '/#authoritative;/c\authoritative;' /etc/dhcp/dhcpd.conf
-    echo "#DHCP ADD">>/etc/dhcp/dhcpd.conf
-    sed -e '/#DHCP ADD/{r /var/www/html/Install/dhcpadd.sun' -e 'd}' -i.bak /etc/dhcp/dhcpd.conf
+    if [ $VERBOSE = "1" ]; then
+        echo "Editing /etc/dhcp/dhcpd.conf"
+    fi
+    if grep -Fxq "DHCP ADD" then
+        echo "done"
+    else
+        sed -i '/option domain-name "example.org";/c\#option domain-name "example.org";' /etc/dhcp/dhcpd.conf
+        sed -i '/option domain-name-servers ns1.example.org, ns2.example.org;/c\#option domain-name-servers ns1.example.org, ns2.example.org;' /etc/dhcp/dhcpd.conf
+        sed -i '/#authoritative;/c\authoritative;' /etc/dhcp/dhcpd.conf
+        echo "#DHCP ADD">>/etc/dhcp/dhcpd.conf
+        sed -e '/#DHCP ADD/{r /var/www/html/INSTALL/dhcpadd.sun' -e 'd}' -i.bak /etc/dhcp/dhcpd.conf
+    fi
 
 #Editing /etc/default/isc-dhcp-server
-    echo "Editing /etc/default/isc-dhcp-server"
+    if [ $VERBOSE = "1" ]; then
+        echo "Editing /etc/default/isc-dhcp-server"
+    fi
     sed -i '#DHCPD_CONF=/etc/dhcp/dhcpd.conf/c\DHCPD_CONF=/etc/dhcp/dhcpd.conf' /etc/default/isc-dhcp-server
     sed -i '/INTERFACES=""/c\INTERFACES="wlan0"' /etc/default/isc-dhcp-server
     service isc-dhcp-server restart
 
 #Editing /etc/hostapd/hostapd.conf
-    echo "Editing /etc/hostapd/hostapd.conf"
+    if [ $VERBOSE = "1" ]; then
+        echo "Editing /etc/hostapd/hostapd.conf"
+    fi
+    if grep -Fxq "#HOSTAPD ADD" then
+        echo "done"
+    else
     echo "#HOSTAPD ADD">>/etc/hostapd/hostapd.conf
-    sed -e '/#HOSTAPD ADD/{r /var/www/html/Install/hostapdadd.sun' -e 'd}' -i.bak /etc/hostapd/hostapd.conf
+    sed -e '/#HOSTAPD ADD/{r /var/www/html/INSTALL/hostapdadd.sun' -e 'd}' -i.bak /etc/hostapd/hostapd.conf
+    fi
 
 #Editing /etc/default/hostapd
-    echo "Editing /etc/default/hostapd"
+    if [ $VERBOSE = "1" ]; then
+        echo "Editing /etc/default/hostapd"
+    fi
     sed -i '/#DAEMON_CONF=""/c\DAEMON_CONF="/etc/hostapd/hostapd.conf"' /etc/default/hostapd
 
 #Editing /etc/init.d/hostapd
-    echo "Editing /etc/init.d/hostapd"
+    if [ $VERBOSE = "1" ]; then
+        echo "Editing /etc/init.d/hostapd"
+    fi
     sed -i '/DAEMON_CONF=/c\DAEMON_CONF=/etc/hostapd/hostapd.conf' /etc/init.d/hostapd
 
 #Editing /etc/sysctl.conf and forwarding wireless connection to ethernet
-    echo "Editing /etc/sysctl.conf forwarding wireless connection to ethernet"
+    if [ $VERBOSE = "1" ]; then
+        echo "Editing /etc/sysctl.conf forwarding wireless connection to ethernet"
+    fi
     sed -i '/#net.ipv4.ip_forward=1/c\net.ipv4.ip_forward=1' /etc/sysctl.conf
     sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
     iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
@@ -222,7 +340,9 @@ else
     sh -c "iptables-save > /etc/iptables/rules.v4"
 
 #Start and setup the daemon so it runs at boot
-    echo "start and setup daemon so it runs at boot"
+    if [ $VERBOSE = "1" ]; then
+        echo "start and setup daemon so it runs at boot"
+    fi
     ifdown wlan0
     ifconfig wlan0 192.168.42.1
     /usr/sbin/hostapd /etc/hostapd/hostapd.conf
@@ -232,6 +352,8 @@ else
     update-rc.d hostapd enable
     update-rc.d isc-dhcp-server enable
 
-    echo "setting flag indicating install was successful"
-    sed -i '/installed=0/c\installed=1' /var/www/html/Install/config.sun
+    if [ $VERBOSE = "1" ]; then
+        echo "setting flag indicating install was successful"
+    fi
+    sed -i '/installed=0/c\installed=1' /var/www/html/INSTALL/config.sun
 fi
