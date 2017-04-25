b7ebaaa3430fe72207f5fa714a6cc397e0dff996
Zanoni Contreras <zanoni@nmsu.edu>
2017-04-25 16:36:41 -0600

    Update file error.

--- UI/AJAX_calls/writeToFileZipCode.php
+++ UI/AJAX_calls/writeToFileZipCode.php
@@ -1,9 +1,9 @@
 <?php  
 $zipCode = $_POST['zipCode'];
-
-if(is_readable("sunShineData.json") == true){
+$jsonFileName = "sunshineData.json";
+if(is_readable($jsonFileName) == true){
 	//Get zip code values from saved file
-	$zipCodeInfo = json_decode(file_get_contents("sunshineData.json"), true);
+	$zipCodeInfo = json_decode(file_get_contents($jsonFileName), true);
 
 	if(isset($zipCodeInfo["{$zipCode}"]) == true){
 		error_log(print_r($zipCodeInfo["{$zipCode}"],true));
