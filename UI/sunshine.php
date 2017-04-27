7a6cea7fd546bf0319564bd59a31e4820d79f2e7
Brian O'Dell <brianodellk@gmail.com>
2017-04-27 06:11:14 -0600

    Began to add tour functionality via bootstrap

--- UI/sunshine.php
+++ UI/sunshine.php
@@ -2,7 +2,7 @@
   //When processing froms from PHP to PHP page we can store error and success messages into session
   //to alert the user of the status of a completed/failed action
   session_start();
-  
+
 	require_once('properties/navigation_menu.php');
   if(isset($_SESSION['error']) == true){
     echo '
@@ -25,9 +25,16 @@
 <!--Page specific JavaScript-->
 <script src="sunshine.js?<?php echo time(); ?>" type="text/javascript"></script>
 
+<!-- Include dependencies for bootstrap-tour -->
+<link href="/properties/third_party_plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+<link href="/properties/third_party_plugins/bootstrap/css/bootstrap-tour.min.css" rel="stylesheet" />
+<script src="/properties/third_party_plugins/jQuery/jquery-3.1.1.min.js" ></script>
+<script src="/properties/third_party_plugins/bootstrap/js/bootstrap.min.js" ></script>
+<script src="/properties/third_party_plugins/bootstrap/js/bootstrap-tour.min.js" ></script>
+
 <!--PAGE CONTENT BEGINS HERE-->
 <div class="container">
-  <!-- 
+  <!--
   	Change this section to include a horizontal banner, this portion of code in the between
   	the grey navigation menu and the clickable tabs
    -->
@@ -40,21 +47,43 @@
     <!--Home div, containing information on project sunshine-->
     <div id="home" class="tab-pane fade in active">
     	<br>
-    	<?php 
+    	<?php
       		require_once('application/home.php');
     	?>
     </div>
     <div id="menu1" class="tab-pane fade">
     	<br>
-      	<?php 
+      	<?php
       		require_once('application/userProfile.php');
        	?>
     </div>
     <div id="menu2" class="tab-pane fade">
     	<br>
-    	<?php 
+    	<?php
       		require_once('application/photo.php');
        	?>
     </div>
   </div>
+  <script>
+     // Instance the tour
+     var tour = new Tour({
+     steps: [
+     {
+       element: "#menu1",
+       title: "Step 1",
+       content: "Go here to change your user profile"
+     },
+     {
+       element: "#menu2",
+       title: "Step 2",
+       content: "Go here to take a picture"
+     }
+     ]});
+
+     // Initialize the tour
+     tour.init();
+
+     // Start the tour
+     tour.start();
+  </script>
 </div>
