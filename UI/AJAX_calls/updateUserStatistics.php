7804703d9702d9e765d9700ce5b92d51e238bb6f
ACN\zanoni <zanoni@email.com>
2017-04-30 11:54:58 -0600

    Update error in reading and writing to user profile when updating user statistics.

--- UI/AJAX_calls/updateUserStatistics.php
+++ UI/AJAX_calls/updateUserStatistics.php
@@ -1,9 +1,9 @@
 <?php  
 $fileJSON = json_decode(file_get_contents('userProfile.json'),true);
-$fileJSON['numberPhotos'] = $fileJSON['numberPhotos'] + 1;   
+$fileJSON['userStatistic']['numberPhotos'] = $fileJSON['userStatistic']['numberPhotos'] + 1;   
 
 if(isset($_POST['sunrise']) == true && $_POST['sunrise'] == 'sunrise'){
-	$fileJSON['numberOfSunrise'] = $fileJSON['numberOfSunrise'] + 1; 
+	$fileJSON['userStatistic']['numberOfSunrise'] = $fileJSON['userStatistic']['numberOfSunrise'] + 1; 
 }
 file_put_contents ('userProfile.json', json_encode($fileJSON));
 
